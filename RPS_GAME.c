bb38285beb838d2be43ffbc0517c8b6648caa6c6
Souvik Ghosh <114401034+Souvik0216J@users.noreply.github.com>
2022-11-26 14:48:13 +0530

    Add files via upload

---
+++ RPS_GAME.c
@@ -0,0 +1,98 @@
+//This Game Developed By Souvik Ghosh.(06-10-2022)
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+int home_screen();
+int game_play();
+
+int main()
+{
+	while(1)
+	{
+		home_screen();
+		game_play();
+	}
+	
+	return 0;
+}
+
+int home_screen()
+{
+	system("cls");
+	printf(" This Game Developed By Souvik Ghosh.(06-10-2022)\n");
+	printf("\n ****************\n");
+	printf(" WELCOME RPS GAME");
+	printf("\n ****************\n\n");
+	printf(" 1...ROCK\n\n 2...PAPER\n\n 3...SCISSORS\n");
+}
+
+int game_play()
+{
+	int c_ch, u_ch;
+	
+	fflush(stdin);	
+	label1:
+	printf("\n\n ENTER YOUR CHOICE: ");
+	scanf("%d", &u_ch);
+	
+	if(u_ch == 1)
+	{
+		printf("\n\n Your Choice ROCK\n");
+	}
+	else if(u_ch == 2)
+	{
+		printf("\n\n Your Choice PAPER\n");
+	}
+	else if(u_ch == 3)
+	{
+		printf("\n\n Your Choice SCISSORS\n");
+	}
+	else
+	{
+		printf(" INVALID CHOICE...");
+		goto label1;
+	}
+	
+	fflush(stdin);	
+	srand(time(0));
+	c_ch = rand() % 3;
+	
+	if(c_ch == 0)
+	{
+		printf(" Computer Choice ROCK\n");
+	}
+	else if(c_ch == 1)
+	{
+		printf(" Computer Choice PAPER\n");
+	}
+	else if (c_ch == 2)
+	{
+		printf(" Computer Choice SCISSORS\n");
+	}
+	
+	fflush(stdin);
+	
+	if(c_ch == u_ch-1)
+	{
+		printf("\n\n\n GMAE DRAW.");
+	}
+	else if(c_ch == 1 && u_ch == 1)
+	{
+		printf("\n\n\n COMPUTER WIN.");
+	}
+	else if(c_ch == 2 && u_ch == 2)
+	{
+		printf("\n\n\n COMPUTER WIN.");
+	}
+	else if(c_ch == 0 && u_ch == 3)
+	{
+		printf("\n\n\n COMPUTER WIN.");
+	}
+	else
+	{
+		printf("\n\n\n YOU WIN");
+	}
+	printf("\n\n\n");
+	system("pause");
+}
